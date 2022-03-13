cd6df5ac3289d262fd7d5f1a143d46eeece2021c
Richard Lyders <Richard@Lyders.com>
2022-03-13 16:28:05 -0500

    Updates

--- src/guideDataStore.js
+++
@@ -1,8 +0,0 @@
-import { readable } from 'svelte/store';
-import customerFeedbackGuideData from "./data/guides/customer-feedback.yaml";
-import turnOnLightsGuideData from './data/guides/turn-on-lights.yaml';
-
-export const guideData = readable(undefined, (set) => {
-    set([customerFeedbackGuideData, turnOnLightsGuideData]);
-    return () => {console.log('guideData.stop');}
-})
